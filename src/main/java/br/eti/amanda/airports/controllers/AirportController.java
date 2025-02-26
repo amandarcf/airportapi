f03a2750b52104a5c96a9291d593f91ac6d12ad9
amandarcf <mndflor03@gmail.com>
2025-02-26 10:55:01 -0300

    Término da Fase 2

---
+++ src/main/java/br/eti/amanda/airports/controllers/AirportController.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.eti.amanda.airports.controllers;
+
+import br.eti.amanda.airports.entities.Airport;
+import br.eti.amanda.airports.service.AirportService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author sesideva
+ */
+
+@RestController
+public class AirportController {
+
+    @Autowired
+    private AirportService airportService;
+
+    /**
+     * Endpoint /airports/airport
+     * Retorna TODOS os aeroportos da base de dados.
+     * @return
+     */
+    @GetMapping("/airport")
+    public List<Airport> findAll() {
+        List<Airport> result = airportService.findAll();
+        return result;
+    }
+}
+
+
